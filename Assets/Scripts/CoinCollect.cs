d9f0644c1d6ee86f6f9798b3e5a07b38271de392
DamonSteam <simon.olivecrona@gmail.com>
2020-09-10 14:01:36 +0200

    Diamond Pickup
    Diamond

--- Assets/Scripts/CoinCollect.cs
+++ Assets/Scripts/CoinCollect.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class CoinCollect : MonoBehaviour
 {
 
     public float rotationspeed;
+    public float collect;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,4 +19,4 @@ public class CoinCollect : MonoBehaviour
     {
         transform.Rotate(0, 0 * Time.deltaTime, rotationspeed);
     }
-}
+    }
