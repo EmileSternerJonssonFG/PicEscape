8fa989065f211dfd50b0ff612f56e37617958b5e
robertarnborg <robert.arnborg@gmail.com>
2020-09-11 10:38:11 +0200

    EventSystems_HealthSystems_InProgress1
    Modular Health and Event Systems in the works... sometimes modular takes time.... ugh

---
+++ Assets/Scripts/HealthSystem/HealthSystem.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor.Events;
+
+[RequireComponent(typeof(GameEventListener))]
+public class HealthSystem : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth = 100;
+
+    public GameEvent onDeathEvent;
+
+    public void RemoveHealth(int healthToRemove = 1)
+    {
+        currentHealth -= healthToRemove;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        CheckHealth();
+    }
+
+    public void RecieveHealth(int healthToRecieve = 1)
+    {
+        currentHealth += healthToRecieve;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+    }
+
+    public void CheckHealth()
+    {
+        if(currentHealth <= 0)
+        {
+            onDeathEvent.RaiseAllEvents();
+        }
+    }
+}
